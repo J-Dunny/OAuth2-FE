c09718ad772ccee94f1231d96de0a56789333415
Jake Dunafon <jlad795@gmail.com>
2022-07-18 20:53:48 -0700

    Add addEmployee

--- src/components/NewEmployee.js
+++ src/components/NewEmployee.js
@@ -1,18 +1,55 @@
 import React from "react";
+import { useState } from "react";
 
-const NewEmployee = () => {
-    const [firstName, setFirstName] = useState()
-    const [lastName, setLastName] = useState()
-    const [email, setEmail] = useState()
+const NewEmployee = (props) => {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
 
+    const submitEmployee = (e) => {
+        e.preventDefault()
+        const newEmployee = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        }
+        props.addEmployee(newEmployee)
+        clearInputs()
+    }
+
+    const clearInputs = () => {
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+    }
 
     return (
         <form>
             <h1>Add New Employee</h1>
-            <input placeholder="First Name" />
-            <input placeholder="Last Name" />
-            <input placeholder="Email" />
-            <button>Submit</button>
+            <input 
+            type="text"
+            name="firstName"
+            placeholder="First Name" 
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+            required
+            />
+            <input 
+            placeholder="Last Name" 
+            type="text"
+            name="lastName" 
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+            required/>
+            <input 
+            placeholder="Email"
+            type="text"
+            name="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required 
+            />
+            <button onClick={e => submitEmployee(e)}>Submit</button>
         </form>
     )
 }
